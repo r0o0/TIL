1c37345fba4833babf43cb7e7de1d12cf968cf1c
r0o0 <rayeoh7@gmail.com>
2020-01-19 18:13:28 +0900

    Update config.js
    - Change title
    - Add description
    - Add lastUpdated in themeConfig

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,15 +2,18 @@ const sidebar = require('./sidebar');
 
 console.log('', sidebar);
 
-const title = 'TIL';
+const title = 'r0o0\'s Blog';
+const description = 'r0o0\'s Programming blog';
 const port = 9090;
 
 module.exports = () => ({
   dest: 'docs/dist',
   title,
+  description,
   port,
   themeConfig: {
     editLinks: true,
+    lastUpdated: 'Last Updated',
     docsDir: 'docs',
     sidebar,
     nav: [{ text: 'Github', link: 'https://github.com/r0o0/TIL' }]
