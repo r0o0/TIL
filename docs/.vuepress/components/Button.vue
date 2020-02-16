355d0353888f6d33c7da8d502c4bbafa63e9fad9
r0o0 <rayeoh7@gmail.com>
2020-02-16 22:55:43 +0900

    Make Button component use native attributes

--- docs/.vuepress/components/Button.vue
+++ docs/.vuepress/components/Button.vue
@@ -1,25 +1,37 @@
 <template>
   <a
     v-if="type === 'link'"
-    role="button"
+    :role="type"
     target="_blank"
     :href="link"
   >
-    <Icon :name="icon" style="width: inherit; height: inherit" />
+    <Icon v-if="icon" :name="icon" style="width: inherit; height: inherit" />
+    <span v-else>{{ text }}</span>
   </a>
-  <button v-else>
-    <Icon :name="icon" />
+  <button
+    v-else
+    v-bind="$attrs"
+    :type="type"
+  >
+    <Icon v-if="icon" :name="icon" />
+    <span v-else>{{ text }}</span>
   </button>
 </template>
 
 <script>
 import Icon from './Icon';
+
 export default {
   name: 'Button',
+  inheritAttrs: false,
   props: {
     icon: String,
-    type: String,
+    type: {
+      default: 'button',
+      type: String,
+    },
     link: String,
+    text: String,
   }
 }
 </script>
